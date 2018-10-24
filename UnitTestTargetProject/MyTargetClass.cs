2132998b43bc705aab5cfb3d161a5f071233f103
664090 <42990883+664090@users.noreply.github.com>
2018-10-24 12:18:39 +0530

    Update MyTargetClass.cs
    last checkin

--- UnitTestTargetProject/MyTargetClass.cs
+++ UnitTestTargetProject/MyTargetClass.cs
@@ -7,7 +7,10 @@
             return pintFirstNumber + pintSecondNumber;
         }
 
-              
+        public int SubractNumbers(int pintFirstNumber, int pintSecondNumber)
+        {
+            return pintFirstNumber - pintSecondNumber;
+        }   
         
 
         public int DivideNumbers(int pintFirstNumber, int pintSecondNumber)
